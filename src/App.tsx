468277113e8c0ce1c23cf86762e861aa6288babf
PC-Ngumoha <pcngumoha@gmail.com>
2024-08-21 07:53:05 +0100

    Setup: setup tailwind CSS on the project with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FC } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: FC = () => {
+    return (
+        <>
+            <section
+                className="m-12 h-screen bg-green-400 items-center justify-center
+              p-12 text-center text-3xl md:p-14 lg:p-16 xl:p-20"
+            >
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Similique, ratione deleniti fugiat architecto officiis,
+                    laboriosam officia sint, sapiente aliquid repellendus
+                    possimus nam nulla excepturi. Ea eos eaque facilis
+                    exercitationem natus.
+                </p>
+            </section>
+        </>
+    );
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
